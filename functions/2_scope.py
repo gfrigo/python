66b4325141f7942e33f0986346a525b699e56e97
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-06-12 15:04:42 -0300

    feat(functions): learn function scope

---
+++ functions/2_scope.py
@@ -0,0 +1,15 @@
+x = 10
+y = 10
+
+def somar():
+  x = 20
+  print(f'{x=}, {y=}')
+  
+  print("Chamando função secundária...")
+  def somar2():
+    global x
+    print(f'{x=}, {y=}')
+
+  somar2()
+
+somar()
