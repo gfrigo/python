ad82fe2442b4daad2ac2b1babb1cac7b75dc768a
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-06-14 11:17:52 -0300

    fix(sets): grammar

--- sets/3_operators.py
+++ sets/3_operators.py
@@ -8,7 +8,7 @@ set_2 = {2, 3, 4, 5, 6}
 set_3 = set_1 | set_2
 print(set_3, "\n")
 
-# Intersecção de 2 conjuntos (itens presentes em amobs)
+# Intersecção de 2 conjuntos (itens presentes em ambos)
 # Operadores: & ou set.intersection(set_2)
 set_3 = set_1 & set_2
 print(set_3, "\n")
