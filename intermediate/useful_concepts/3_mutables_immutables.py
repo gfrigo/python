193ba77e6af714730d2e99bed3719b59e3ca7e3d
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-06-23 08:38:11 -0300

    feat(useful_concepts): immutables and mutables types in python language

---
+++ intermediate/useful_concepts/3_mutables_immutables.py
@@ -0,0 +1,15 @@
+# Mutável: Pode ser modificado
+# Imutável: Não pode ser modificado
+
+# Itens mutáveis na linguagem Python:
+lista = [0, 1]
+dicionario = {"chave": "valor"}
+conjunto = set(1, 2)
+
+# Itens imutáveis na linguagem Python:
+tupla = (1, 2)
+texto = "Olá, mundo!" 
+inteiro = 1
+flutuante = 1.1
+nada = None
+falso = True
