9c1259cdfa85f9e522774e0818e9bb6efc1f66cd
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-06-26 12:51:41 -0300

    feat(useful_concepts): imports

---
+++ intermediate/useful_concepts/8_import.py
@@ -0,0 +1,27 @@
+# Módulos: arquivos.py
+# Pacotes: pastas contendo os arquivos.py
+
+# Formas de importar módulos, pacotes e bibliotecas no python
+# Obs: Procurar deixar explícito funcionalidades e módulos no código, evitando confusão
+
+"""
+- as == alias (apelido)
+- Obs: Usar quando for uma convenção da comunidade
+- Obs: Não faz tanto sentido usar para funcionalidades criadas por você (causa confusão)
+"""
+import pandas as pd 
+
+"""
+- import lib == Importar biblioteca completa
+- from lib import function == importar funcionalidade específica da biblioteca/módulo
+- Obs: "read_csv" é como uma variável no código, não pode ser utilizado outro nome,
+  pois, irá sobrescrever o "read_csv" do pandas
+"""
+from pandas import read_csv
+
+
+"""
+- from lib import *
+- Obs: Importa tudo da biblioteca/módulo (não é uma boa prática)
+"""
+from pandas import *
